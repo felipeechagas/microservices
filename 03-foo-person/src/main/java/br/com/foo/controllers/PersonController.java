52ede99a6075f0a91f5ecba58e56f79933953cc2
Felipe Chagas <felipechagasferreira@gmail.com>
2023-05-01 11:51:58 -0300

    Fix Directory

---
+++ 03-foo-person/src/main/java/br/com/foo/controllers/PersonController.java
@@ -0,0 +1,54 @@
+package br.com.felipe.foo.controllers;
+
+import br.com.felipe.foo.model.Person;
+import br.com.felipe.foo.services.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+  @Autowired
+  private PersonService personService;
+
+  @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+  public List<Person> findAll(){
+    return personService.findAll();
+  }
+
+  //Retorna person pelo id
+  @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+  public Person findById(@PathVariable("id") String id){
+    return personService.findById(id);
+  }
+
+  @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  public Person create(@RequestBody Person person){
+    return personService.create(person);
+  }
+
+  @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  public Person update(@RequestBody Person person){
+    return personService.update(person);
+  }
+
+  //Deleta person pelo id
+  @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+  public void delete(@PathVariable("id") String id){
+    personService.delete(id);
+  }
+
+}
+
+/**
+ *  Para verificar o resultado no browser,
+ *  digite a url: localhost:8080/sum/num1/num2
+ *  substitua num1 e num2 pelo numero desejado
+ *
+ *  Caso seja digitado letra ou caracteres diferentes, será apresentado uma mensagem de erro.
+ */
